b3ee3329a676a7021a5ba91932b596094dde5ca0
Ivan Marahovskii <marahovskii81@gmail.com>
2023-12-09 13:26:14 +0500

    music-app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { MainPage } from './pages/main-page';
+import './App.css';
+
+function App() {
+  return (
+    <div className="wrapper container">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/pages/main-page/index.jsx
@@ -0,0 +1,7 @@
+import { tracksList } from '../../assets/tracks-list';
+
+function MainPage() {
+  return <>Главная страница</>;
+}
+
+export { MainPage };
